7917fc52a45e26f8f760ba0a95544a1023b52e6c
Shreya Sridhar <shreyamohak@outlook.com>
2020-10-30 00:26:12 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,18 @@
+def using_concat(arr1,arr2)
+  arr1.concat(arr2)
+  
+def using_insert(arr,ele)
+  arr.insert(ele,4)
+  
+def using_uniq(arr)
+  arr.uniq
+  
+def using_flatten(arr)
+  arr.flatten 
+  
+def using_delete(arr,string)
+  arr.delete(string)
+  
+def using_delete_at(arr,integer)
+  arr.delete_at(integer)
+  
